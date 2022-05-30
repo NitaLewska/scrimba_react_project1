25c1da8cb5053cc67461c7a4bcd6e31720f6024e
NitaLewska <Nita@ya.ru>
2022-05-31 00:22:13 +0300

    init download

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import './Main.css'
+
+export default function Main() {
+    return(
+        <div className="main">
+            <h1 className="main--heading">Fun facts about React</h1>
+            <ul className="main--list">
+                <li className="main--list_item">Was first released in 2013</li>
+                <li className="main--list_item">Was originally created by Jordan Walke</li>
+                <li className="main--list_item">Has well over 100K stars on GitHub</li>
+                <li className="main--list_item">Is maintained by Facebook</li>
+                <li className="main--list_item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from'../img/reactjs-icon.png'
+import './Navbar.css'
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={logo} alt="logo" className="nav--logo"/>
+            <h2 className="nav--logo_text">ReactFacts</h2>
+            <p className="nav--description">React Course - Project 1</p>
+        </nav>
+    )
+}
