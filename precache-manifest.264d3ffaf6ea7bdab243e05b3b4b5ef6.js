1c26a1399b590633981b1dde24c3bb3cbb68e01e
NitaLewska <Nita@ya.ru>
2022-05-31 19:52:32 +0300

    Updates

---
+++ precache-manifest.264d3ffaf6ea7bdab243e05b3b4b5ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0cf27160b179a38a23",
+    "url": "/scrimba_react_project1/static/css/main.579dc8bf.chunk.css"
+  },
+  {
+    "revision": "8f0cf27160b179a38a23",
+    "url": "/scrimba_react_project1/static/js/main.8f0cf271.chunk.js"
+  },
+  {
+    "revision": "b4a0a8d5f585f948c124",
+    "url": "/scrimba_react_project1/static/js/1.b4a0a8d5.chunk.js"
+  },
+  {
+    "revision": "85f442d28420b0be51c9",
+    "url": "/scrimba_react_project1/static/js/runtime~main.85f442d2.js"
+  },
+  {
+    "revision": "0cf4490a61f4fb5ebc37859d26a1fce2",
+    "url": "/scrimba_react_project1/index.html"
+  }
+];
